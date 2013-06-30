ee069c7f0d0f45494b9cea1aab38c540d2df40f8
avishay <talavishay@gmail.com>
2013-06-30 18:19:48 +0300

    V0.441  - added michael biran bio & more

--- sites/all/modules/ckeditor_custom/ckeditor_custom_config.js
+++ sites/all/modules/ckeditor_custom/ckeditor_custom_config.js
@@ -3,27 +3,9 @@ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.config.fontSize_sizes ='12 פיקסל/12px;'        +
-'13 פיקסל/13px;'+
-'14 פיקסל/14px;'+
-'16 פיקסל/16px;'+
-                                    '18 פיקסל/18px;'+
-                                    '20 פיקסל/20px;'+
-                                    '22 פיקסל/22px;'+
-                                    '24 פיקסל/24px;'+
-                                    '26 פיקסל/26px;'+
-                                    '28 פיקסל/28px;'+
-                                    '30 פיקסל/30px;'+   
-                                    '32 פיקסל/32px;'+
-                                    '40 פיקסל/40px;'+
-                                    '50 פיקסל/50px;'+
-                                    '60 פיקסל/60px;';
-                            
 CKEDITOR.config.contentsLangDirection='ltr';
-CKEDITOR.config.contentsCss = '/sites/all/themes/mobility/css/mobility-text.css' 
-CKEDITOR.editorConfig = function( config )
-{
-        config.extraCss = "body{min-width:auto !important}";
+
+CKEDITOR.editorConfig = function( config ){
      
   // config.styleSet is an array of objects that define each style available
   // in the font styles tool in the ckeditor toolbar
@@ -35,15 +17,7 @@ CKEDITOR.editorConfig = function( config )
         // Each style is an object whose properties define how it is displayed
         // in the dropdown, as well as what it outputs as html into the editor
         // text area.
-        { name : 'פיסקה'   , element : 'p' },
-//        { name : 'padding', element : 'div', attributes : { 'style' : 'padding:2em;    ' } },
-        { name : 'כותרת 1'  , element : 'h1' },
-        { name : 'כותרת 2'   , element : 'h2' },
-        { name : 'כותרת 3'   , element : 'h3' },
-//        { name : 'כותרת 4'   , element : 'h4' },
-//        { name : 'Float Right', element : 'div', attributes : { 'style' : 'float:right;' } },
-//        { name : 'Float Left', element : 'div', attributes : { 'style' : 'float:left;margin-right: 2em;' } },
-//        { name : 'Preformatted Text', element : 'pre' },
+        { name : 'publication category'   , element : 'h6', attributes : { 'style' : 'color:#620B01;    ' } }
   ];
 
-}
+};
